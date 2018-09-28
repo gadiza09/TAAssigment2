e932837b45071af3201a266bee6c61c2bb488017
Gadiza Pribadi <gadiza09@Gadizas-MacBook-Pro.local>
2018-09-28 22:10:16 +0700

    First Commit

--- CupsNBall.py
+++ CupsNBall.py
@@ -1,21 +1,21 @@
-moves = str(input("Please enter the moves (a/b/c): "))
-list = ["b", " ", " "]
-for i in range(len(moves)):
-    if moves[i] == "a":
-        temporary = list[0]
-        list[0] = list[1]
-        list[1] = temporary
-    elif moves[i] == "b":
-        temporary = list[1]
-        list[1] = list[2]
-        list[2] = temporary
-    elif moves[i] == "c":
-        temporary = list[0]
-        list[0] = list[2]
-        list[2] = temporary
+options = str(input("Please enter the moves (a/b/c): "))
+spaces = ["b", " ", " "]
+for i in range(len(options)):
+    if options[i] == "a":
+        temporary = spaces[0]
+        spaces[0] = spaces[1]
+        spaces[1] = temporary
+    elif options[i] == "b":
+        temporary = spaces[1]
+        spaces[1] = spaces[2]
+        spaces[2] = temporary
+    elif options[i] == "c":
+        temporary = spaces[0]
+        spaces[0] = spaces[2]
+        spaces[2] = temporary
     else:
         print("Please choose only a/b/c.")
-print(list)
+print(spaces)
 for j in range(3):
-    if list[j] == "b":
+    if spaces[j] == "b":
         print("The ball is in position", j+1)
